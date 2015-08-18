16c6270ba1a90bbc15eee61112587e7b0f79da7e
topleft <pete.james.jeffryes@gmail.com>
2015-08-18 15:02:31 -0600

    angular calculating total and profit

--- js/ang.js
+++ js/ang.js
@@ -1,7 +1,18 @@
   console.log("sanity")
 
   var bitcoinCalculator = angular.module('bitcoinCalculator', []);
-  bitcoinCalculator.controller('bitcoinController', ['$scope', function ($scope) {
-    // attaching 0 to the DOM
-    $scope.somenumber = 0;
-  }]);
+
+  bitcoinCalculator.controller('bitcoinController', function ($scope, $http) {
+      $http.get("https://bitpay.com/api/rates")
+      .success(function(data){
+        $scope.rates = data;
+        for (var i = 0; i < data.length; i++) {
+          if(data[i].code=="USD"){
+            $scope.currRate = data[i].rate;
+          }
+        }
+        $scope.initialAmt = 5000;
+        $scope.newAmt = function(forecastPrice){return $scope.initialAmt/$scope.currRate * forecastPrice}
+        $scope.profit = function(forecastPrice){return ($scope.initialAmt/$scope.currRate *forecastPrice)-$scope.initialAmt}
+      })
+  });
